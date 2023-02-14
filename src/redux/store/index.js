9fc714d7f801937643201633fccdd47d2b78599e
DianaBerte <dianaberte.go@gmail.com>
2023-02-14 14:35:54 +0100

    Added redux-persist

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,14 +1,34 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  combineReducers,
+  // getDefaultMiddleware,
+} from "@reduxjs/toolkit";
 import favouriteReducer from "../reducers/favourite";
 import jobReducer from "../reducers/job";
+import { persistStore, persistReducer } from "redux-persist";
+import localStorage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  storage: localStorage,
+  key: "root",
+};
 
 const bigReducer = combineReducers({
   favourite: favouriteReducer,
   job: jobReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, bigReducer);
+
 const store = configureStore({
-  reducer: bigReducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    });
+  },
 });
 
-export default store;
+const persistedStore = persistStore(store);
+
+export { store, persistedStore };
